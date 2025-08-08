e633d7c4de22147ef4ca40c99670f3e006da0a9b
MorseWayne <2539720515@qq.com>
2025-08-08 05:09:44 +0000

    config: 更新网站base路径

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/programming_journey/",
 
   lang: "zh-CN",
   title: "编程之旅✨",
